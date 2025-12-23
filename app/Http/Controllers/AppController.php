6b96811929e05c436b6793ce601742b0c60309c3
Teguhriyadi <ilham.teguh55@gmail.com>
2025-12-24 00:01:06 +0700

    Update

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,14 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Excel\IncomeDataReadFilter;
-use Illuminate\Http\Request;
-use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use App\Excel\IncomeHeaderReadFilter;
 use App\Models\Platform;
-use App\Models\UploadsData;
-use App\Models\UploadsFile;
 use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\Shared\Date as ExcelDate;
 
@@ -23,7 +16,6 @@ class AppController extends Controller
 
             $data["platform"] = Platform::where("status", "1")->count();
             $data["seller"] = Platform::where("status", "1")->count();
-            $data["banyak_upload"] = UploadsFile::count();
 
             DB::commit();
 
