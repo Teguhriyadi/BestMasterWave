7df87b4240982287a5a9ed2f910ff8095778f7c6
Teguhriyadi <ilham.teguh55@gmail.com>
2026-01-11 21:52:00 +0700

    Create Migration Permissions

---
+++ database/migrations/2026_01_11_205824_create_role_permission_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->uuid('role_id');
+            $table->uuid('permission_id');
+            $table->uuid('divisi_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('role')->cascadeOnDelete();
+            $table->foreign('permission_id')->references('id')->on('permissions')->cascadeOnDelete();
+            $table->foreign('divisi_id')->references('id')->on('divisi')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_permission');
+    }
+};
