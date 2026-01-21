d78012776452d4aedff61f7c98fcac1407213d1f
Teguhriyadi <ilham.teguh55@gmail.com>
2026-01-21 16:57:29 +0700

    Update Permissions

--- app/Http/Repositories/DivisiRepository.php
+++ app/Http/Repositories/DivisiRepository.php
@@ -53,13 +53,8 @@ class DivisiRepository
 
     public function getRolesByDivisi(string $divisionId)
     {
-        return Divisi::with([
-            'roles' => function ($q) use ($divisionId) {
-                $q->select('id', 'nama_role', 'divisi_id')
-                  ->where('divisi_id', AuthDivisi::id());
-            }
-        ])
-        ->findOrFail($divisionId)
-        ->roles;
+        return Divisi::with('roles:id,nama_role')
+            ->findOrFail($divisionId)
+            ->roles;
     }
 }
